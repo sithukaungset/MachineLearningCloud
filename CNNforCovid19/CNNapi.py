19ee9e6206804b065b5d48aa063b80e683a5a151
Sithu Kaungset <sithukaungset@MacBook-Air.local>
2022-11-02 17:08:26 +0900

    fix Machine Learning model

---
+++ CNNforCovid19/CNNapi.py
@@ -0,0 +1,46 @@
+# Install libraries
+from inspect import trace
+from flask import Flask, request, jsonify
+import joblib
+import traceback
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+
+@app.route('/prediction', methods=['POST'])
+# define function
+def predict():
+    if lr:
+        try:
+            json_ = request.json
+            print(json_)
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=rnd_columns, fill_value=0)
+
+            predict = list(lr.predict(query))
+
+            return jsonify({'prediction': str(predict)})
+
+        except:
+            return jsonify({'trace': traceback.format_exc()})
+
+    else:
+        print("Model is not good")
+        return ('Model is not good')
+
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1])
+    except:
+        port = 12345
+
+        lr = joblib.load("randomfs.pkl")
+        print("Model Loaded")
+        rnd_columns = joblib.load("rnd_columns.pkl")
+        # Load "rnd_columns.pkl"
+        print("Model columns loaded")
+
+        app.run(port=port, debug=True)
---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# FROM jupyter/scipy-notebook
+
+# RUN pip install joblib
+
+# COPY train.csv ./train.csv
+# COPY test.csv ./test.csv
+
+# COPY train.py ./train.py
+# COPY inference.py ./inference.py
+
+# RUN python3 train.py
+
+FROM jupyter/scipy-notebook
+
+RUN pip install 
