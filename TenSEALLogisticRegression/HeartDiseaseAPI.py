d5a849c12cd3218d94a2d137b57a39ee3cddc272
sithukaungset <sithukaungset27@gmail.com>
2022-11-05 23:05:33 +0900

    ML models

---
+++ TenSEALLogisticRegression/HeartDiseaseAPI.py
@@ -0,0 +1,48 @@
+# Install libraries
+from inspect import trace
+import sys
+from flask import Flask, request, jsonify
+import joblib
+import traceback
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+
+@app.route('/prediction/heart', methods=['POST'])
+# define function
+def predict():
+    if lrc:
+        try:
+            json_ = request.json
+            print(json_)
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=lrc_columns, fill_value=0)
+
+            predict = list(lrc.predict(query))
+
+            return jsonify({'prediction': str(predict)})
+
+        except:
+            return jsonify({'trace': traceback.format_exc()})
+
+    else:
+        print("Model is not good")
+        return ('Model is not good')
+
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1])
+    except:
+        port = 3005
+
+        lrc = joblib.load("heartLR.pkl")
+        print("Model Loaded")
+        lrc_columns = joblib.load("lrc_columns.pkl")
+        # Load "rnd_columns.pkl"
+        print("Model columns loaded")
+
+        # app.run(host="203.247.240.226", port=port, debug=True)
+        app.run(host="203.247.240.226",port=port, debug=True)
